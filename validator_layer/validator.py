424cb0833518ead8fd2b7d254fbf7015ba3c3da9
thealepo <sanchezc.alexe@gmail.com>
2025-10-19 19:15:32 -0400

    fixed original issue from last commit, left notes on what to work on in validation_layer/validator.py

--- validator_layer/validator.py
+++ validator_layer/validator.py
@@ -1,24 +1,17 @@
 import re
-from enum import Enum
+from enum import Enum , auto
 from dataclasses import dataclass
 from typing import Optional , Dict
 # notebook imports
 from lean_dojo import (
     LeanGitRepo , Theorem , Dojo , TacticState , ProofFinished , LeanError
 )
-import sys
-import os
-
-from numpy import result_type
-
-sys.path.append(os.path.join(os.path.dirname(__file__) , '..'))
-from llm_layer.data_structures.base import LeanGoalState
 
 
-class ValidationResult:
-    VALID = ...
-    INVALID = ...
-    PROOF_FINISHED = ...
+class ValidationResult(Enum):
+    VALID = auto()
+    INVALID = auto()
+    PROOF_FINISHED = auto()
 
 @dataclass
 class ValidationResponse:
@@ -27,10 +20,12 @@ class ValidationResponse:
     error_msg = Optional[str]
 
 class LeanValidator:
-    def __init__(self , repo , file_path , theorem_name):
+    def __init__(self , repo: LeanGitRepo , file_path , theorem_name):
         self.theorem = Theorem(repo , file_path , theorem_name)
         self.dojo = Dojo(self.theorem)
-        self.initial_state = self.dojo.get_initial_state()  # for some reason this method is not working anymore, must check docs
+        self.initial_state = Optional[TacticState] = None
+
+    # we have to find a new way to initialize a tactic state. probably should look into old docs and see how the old get_initial_state() worked, and replicate.
     
     def validate_tactic(self , current_state , tactic_code):
         ''' def search(state: TacticState , depth: int) -> Optional(Proof) '''
@@ -40,3 +35,5 @@ class LeanValidator:
             return ValidationResponse(result_type=ValidationResult.PROOF_FINISHED)
         elif isinstance(result , TacticState):
             return ValidationResponse(result_type=ValidationResult.VALID)
+        else:
+            return ValidationResponse(result_type=ValidationResult.INVALID)
