33a1557ea84a70c7ff51df213dfacd3bb2383341
thealepo <sanchezc.alexe@gmail.com>
2025-10-24 04:57:54 -0400

    search layer skeleton fleshed out. need FCM for implementation in its own folder.

--- search_layer/search.py
+++ search_layer/search.py
@@ -4,17 +4,30 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__) , '..'))
 from llm_layer.data_structures.base import LeanGoalState , SearchConstraints , TacticCandidate
+from llm_layer.models.lean_generator_model import LeanGenerator
+from llm_layer.models.reasoning_model import MathReasoner
 
 class Search:
-    def __init__(self):
-        ...
+    def __init__(self , reasoner: MathReasoner , generator: LeanGenerator):
+        self.reasoner = reasoner
+        self.generator = generator
     
     def search():
         # dfs for now>
         # candidates -> goal
-        candidates = TacticCandidate
+        
+        # STEP 1: call MathReasoner to generate SearchConstraints
 
-        for i,candidate in enumerate(candidates):
-            # call validator
-            ...
-            # create new proof state?
+        # STEP 2: check with FCM
+
+        # STEP 3: call LeanGenerator
+
+        # STEP 4: take TacticCandidate(s) and validate
+
+        # STEP 5: learning loop
+            # VALID: new LeanGoalState, strengthen weights on FCM, repeat 1
+            # INVALID: weaken weights on FCM, try next candidate from 3 and repeat 4. 
+                        # if all has been exhausted, repeat from 1 and add to FailedTactics
+            # PROOF_FINISHED: yay!
+
+        pass
