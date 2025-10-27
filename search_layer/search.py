e4ec49472cba9a8cb574ed495188622693db5877
thealepo <sanchezc.alexe@gmail.com>
2025-10-27 17:43:01 -0400

    fixed constructor in Search class. skeleton more or less done, havent checked the types yet however due to the need for a translator.

--- search_layer/search.py
+++ search_layer/search.py
@@ -6,39 +6,51 @@ sys.path.append(os.path.join(os.path.dirname(__file__) , '..'))
 from llm_layer.data_structures.base import LeanGoalState , SearchConstraints , TacticCandidate , FailedTactic
 from llm_layer.models.lean_generator_model import LeanGenerator
 from llm_layer.models.reasoning_model import MathReasoner
-from validator_layer.validator import LeanValidator
+from validator_layer.validator import LeanValidator, ValidationResult
 
 class Search:
-    def __init__(self , reasoner: MathReasoner , generator: LeanGenerator):
+    def __init__(self , reasoner: MathReasoner , generator: LeanGenerator , validator: LeanValidator):
         self.reasoner = reasoner
         self.generator = generator
-        self.validator = LeanValidator
+        self.validator = validator
+        # self.fcm
     
     def search(self , state: LeanGoalState , depth: int):
         # dfs for now>
         # candidates -> goal
-        failures = []  # placeholder
+        failures = list[FailedTactic] = []
 
         
-        # STEP 1: call MathReasoner to generate SearchConstraints
-        constraints = self.reasoner.generate_search_constraints(
-            goal_state=state,
-            failed_attempts=failures,
-        )
-        # STEP 2: check with FCM
-        ...
-        # STEP 3: call LeanGenerator
-        candidates = self.generator.generate_candidates(
-            goal_state=state,
-            constraints=constraints
-        )
-        # STEP 4: take TacticCandidate(s) and validate
-        for candidate in candidates:
-            next_state = self.validator(...)
-        # STEP 5: learning loop
-            # VALID: new LeanGoalState, strengthen weights on FCM, repeat 1
-            # INVALID: weaken weights on FCM, try next candidate from 3 and repeat 4. 
-                        # if all has been exhausted, repeat from 1 and add to FailedTactics
-            # PROOF_FINISHED: yay!
+        while True:
+            # STEP 1: call MathReasoner to generate SearchConstraints
+            constraints = self.reasoner.generate_search_constraints(
+                goal_state=state,
+                failed_attempts=failures,
+            )
+            # STEP 2: check with FCM
+            ...
+            # STEP 3: call LeanGenerator
+            candidates = self.generator.generate_candidates(
+                goal_state=state,
+                constraints=constraints
+            )
+            # STEP 4: take TacticCandidate(s) and validate
+            for candidate in candidates:
+                response = self.validator.validate_tactic(
+                    current_state='''current tactic state''',
+                    tactic_code='''translation code'''
+                )
 
-        pass
+                if response.result_type == ValidationResult.PROOF_FINISHED:
+                    ...
+                elif response.result_type == ValidationResult.VALID:
+                    next_state = LeanGoalState(...)
+                    
+                
+            # STEP 5: learning loop
+                # VALID: new LeanGoalState, strengthen weights on FCM, repeat 1
+                # INVALID: weaken weights on FCM, try next candidate from 3 and repeat 4. 
+                            # if all has been exhausted, repeat from 1 and add to FailedTactics
+                # PROOF_FINISHED: yay!
+
+            pass
