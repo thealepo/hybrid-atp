daae48fdda62881b5715ff27fa2d7fafdad65dc7
thealepo <sanchezc.alexe@gmail.com>
2025-10-31 11:01:35 -0400

    began a 'sandbox validator'

--- validator_layer/new_validator.py
+++ validator_layer/new_validator.py
@@ -1,3 +1,26 @@
 # new validator... we might need different modules
 # this is beyond complicated
 
+import os
+from enum import Enum , auto
+from dataclasses import dataclass
+from typing import Optional
+# notebook imports
+from lean_dojo import (
+    LeanGitRepo , Theorem , Dojo , TacticState , ProofFinished
+)
+
+
+class ValidationResult(Enum):
+    VALID = auto()
+    INVALID = auto()
+    PROOF_FINISHED = auto()
+
+@dataclass
+class ValidationResponse:
+    result_type: ValidationResult
+    new_state: Optional[TacticState] = None
+    error_msg: Optional[str] = None
+
+class SandboxValidator:
+    ...
