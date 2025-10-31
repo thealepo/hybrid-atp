04de8125de29278c8b0e1578cfbec99279525aec
thealepo <sanchezc.alexe@gmail.com>
2025-10-30 23:28:35 -0400

    refactored and restructured validator.py

--- validator_layer/validator.py
+++ validator_layer/validator.py
@@ -1,18 +1,12 @@
-import re
-import sys , os
-from dotenv import load_dotenv
+import os
 from enum import Enum , auto
 from dataclasses import dataclass
-from typing import Optional , Dict
+from typing import Optional
 # notebook imports
 from lean_dojo import (
-    LeanGitRepo , Theorem , Dojo , TacticState , ProofFinished , LeanError , TracedRepo
+    LeanGitRepo , Theorem , Dojo , TacticState , ProofFinished, TracedRepo
 )
 
-load_dotenv()
-COMMIT_HASH = os.getenv('LEANGITREPO_COMMIT_HASH')
-
-os.environ["GITHUB_TOKEN"] = os.getenv("GITHUB_TOKEN")
 
 class ValidationResult(Enum):
     VALID = auto()
@@ -26,32 +20,36 @@ class ValidationResponse:
     error_msg: Optional[str] = None
 
 class LeanWrapper:
-    def __init__(self , repo_path: str = 'https://github.com/leanprover-community/mathlib4' , commit: str = '68abb997e5168875b210d31c6c31588b0998db03'):
-        self.repo_path = repo_path
-        self.commit = commit
-
+    def __init__(self , repo_url: str = 'https://github.com/leanprover-community/mathlib4' , commit: str = '68abb997e5168875b210d31c6c31588b0998db03'):
         self.repo = LeanGitRepo(
-            url=self.repo_path,
-            commit=self.commit
+            url=repo_url,
+            commit=commit
         )
 
-    def get_theorem(self , file_path: str , theorem_name: str):
+    def get_theorem(self , file_path: str , theorem_name: str) -> Theorem:
         return Theorem(
             repo=self.repo,
             file_path=file_path,
-            theorem_name=theorem_name
+            full_name=theorem_name
         )
 
-    def get_dojo(self , theorem):
+    def get_dojo(self , theorem) -> Dojo:
         return Dojo(theorem)
 
 class LeanValidator:
-    def __init__(self , repo_path: str , file_path: str , theorem_name: str):
-        self.lean_wrapper = LeanWrapper(repo_path=repo_path)
+    def __init__(self , repo_url: str , file_path: str , theorem_name: str):
+        self.lean_wrapper = LeanWrapper(repo_url=repo_url)
         self.theorem = self.lean_wrapper.get_theorem(file_path=file_path , theorem_name=theorem_name)
-    
+
+        # traced repo
+        self.traced_repo = TracedRepo(self.wrapper.repo)
+        self.traced_theorem = self.traced_repo.get_traced_theorem(self.theorem)
+
         # load dojo
         self.dojo = self.lean_wrapper.get_dojo(self.theorem)
+
+        traced_tactics = getattr(self.traced_theorem , 'traced_tactics' , [])
+        self.initial_state = traced_tactics[0].tactic_state if traced_tactics else None
 
     def validate_tactic(self , current_state: TacticState , tactic_code) -> ValidationResponse:
         ''' def search(state: TacticState , depth: int) -> Optional(Proof) '''
