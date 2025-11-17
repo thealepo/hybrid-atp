5544f4eed4cdc07c04c40ad7c116dfc350594c4a
thealepo <sanchezc.alexe@gmail.com>
2025-11-17 00:39:17 -0500

    90% of the way done

--- validation_layer/test.py
+++ validation_layer/test.py
@@ -1,9 +1,27 @@
 from lean_dojo import *
 from validator import LeanDojoValidator
+import time
 
 repo_url = "https://github.com/leanprover-community/mathlib4"
-file_path = "Mathlib/Algebra/Ring/Defs.lean"
-theorem_name = "add_comm"
+file_path = "Mathlib/Data/Nat/Basic.lean"
+theorem_name = "Nat.add_comm"
 
-validator = LeanDojoValidator(repo_url, file_path, theorem_name)
-print(validator.state_tactic_pairs)
+try:
+    start_time = time.time()
+
+    validator = LeanDojoValidator(repo_url , file_path , theorem_name)
+
+    state = validator.get_initial_state()
+    print(state.pp)
+
+    tactic = "rw [Nat.add_comm]"
+    print(tactic)
+
+    response = validator.validate(state , tactic)
+
+    print(response.result_type)
+
+except Exception as e:
+    print(e)
+
+print(f"Time taken: {time.time() - start_time} seconds")
--- validation_layer/validator.py
+++ validation_layer/validator.py
@@ -18,18 +18,14 @@ class LeanDojoValidator:
     def __init__(self , repo_url: str , file_path: str , theorem_name: str):
         self.repo = LeanGitRepo(repo_url , '48b7eb4c1789b21a9804ba876d458c2e95b45fd7')  # hash of repo, put on environment variable?
 
-        traced_repo = trace(self.repo)
-        theorems = traced_repo.get_traced_theorems()
+        self.theorem = Theorem(self.repo , file_path , theorem_name)
 
-        state_tactic_pairs = []
-        for theorem in theorems:
-            for t in theorem.get_traced_tactics():
-                state_tactic_pairs.append((t.state_before() , t.tactic()))
-
-        self.state_tactic_pairs = state_tactic_pairs
+        self.dojo , self.initial_state = Dojo(
+            self.theorem
+        ).__enter__()
 
     def get_initial_state(self) -> TacticState:
-        ...
+        return self.initial_state
 
     def validate(self , state: TacticState , tactic_code: str) -> ValidationResponse:
         next_state = self.dojo.run_tac(state , tactic_code)
@@ -41,3 +37,6 @@ class LeanDojoValidator:
             return ValidationResponse(ValidationResult.INVALID , next_state=None , error=str(next_state))
 
         return ValidationResponse(ValidationResult.VALID , next_state=next_state)
+
+    def close(self):
+        self.dojo.__exit__(None , None , None)
