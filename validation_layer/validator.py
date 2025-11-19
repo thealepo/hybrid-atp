e34698eb6eb0f82f00ef5a3fe39cc074d4b32b18
thealepo <sanchezc.alexe@gmail.com>
2025-11-19 11:47:55 -0500

    generalized local math library (mathlib4, miniF2F, etc.)

--- validation_layer/validator.py
+++ validation_layer/validator.py
@@ -15,8 +15,8 @@ class ValidationResponse:
     error: Optional[str] = None
 
 class LeanDojoValidator:
-    def __init__(self , repo_url: str , file_path: str , theorem_name: str):
-        self.repo = LeanGitRepo(repo_url , 'be5e7c616339bf31dbc732277a12edd5ef8bdb92')  # hash of repo, put on environment variable?
+    def __init__(self , repo_url: str,  file_path: str , theorem_name: str , repo_hash: str = 'be5e7c616339bf31dbc732277a12edd5ef8bdb92'):
+        self.repo = LeanGitRepo(repo_url , repo_hash)  # hash of repo, put on environment variable?
 
         self.theorem = Theorem(self.repo , file_path , theorem_name)
 
