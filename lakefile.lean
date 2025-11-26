d4b9894f3dfb46447749e8d59f7890d33f5bc4d7
thealepo <sanchezc.alexe@gmail.com>
2025-11-26 18:53:14 -0500

    temporary debug added

---
+++ lakefile.lean
@@ -0,0 +1,10 @@
+import Lake
+open Lake DSL
+
+package «hybrid_atp» where
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib HybridAtp
