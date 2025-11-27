e5143afda748a28520c2ce2deae74d7700a85053
thealepo <sanchezc.alexe@gmail.com>
2025-11-26 19:45:05 -0500

    new implementation of validator (third time)

--- lean_core/Main.lean
+++
@@ -1,4 +0,0 @@
-import HybridAtp
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- validation_layer/validator.py
+++ validation_layer/validator.py
@@ -14,37 +14,13 @@ class ValidationResponse:
     next_state: Optional[TacticState] = None
     error: Optional[str] = None
 
-class LeanDojoValidator:
-    def __init__(self ,  file_path: str , theorem_name: str , repo_url: str = "https://github.com/leanprover-community/mathlib4" , repo_hash: str = 'b5eba595428809e96f3ed113bc7ba776c5f801ac'):
-        self.repo = LeanGitRepo(repo_url , repo_hash)
+def validate_tactic(dojo: Dojo , state: TacticState , tactic: str) -> ValidationResponse:
+    result = dojo.run_tac(state , tactic)
 
-        self.theorem = Theorem(self.repo , file_path , theorem_name)
-
-        self.dojo = None
-        self.initial_state = None
-
-    def initialize(self):
-        self.dojo , self.initial_state = Dojo(
-            self.theorem
-        ).__enter__()
-        return self.initial_state
-
-    def get_initial_state(self) -> TacticState:
-        if self.initial_state is None:
-            return self.initialize()
-        return self.initial_state
-
-    def validate(self , state: TacticState , tactic_code: str) -> ValidationResponse:
-        next_state = self.dojo.run_tac(state , tactic_code)
-
-        if isinstance(next_state , ProofFinished):
-            return ValidationResponse(ValidationResult.PROOF_FINISHED , next_state=None)
-        
-        if isinstance(next_state , LeanError):
-            return ValidationResponse(ValidationResult.INVALID , next_state=None , error=str(next_state))
-
-        return ValidationResponse(ValidationResult.VALID , next_state=next_state)
-
-    def close(self):
-        if self.dojo:
-            self.dojo.__exit__(None , None , None)
+    if isinstance(result , ProofFinished):
+        return ValidationResponse(ValidationResult.PROOF_FINISHED)
+    elif isinstance(result , LeanError):
+        return ValidationResponse(ValidationResult.INVALID , error=result.error)
+    elif isinstance(result , TacticState):
+        return ValidationResponse(ValidationResult.VALID , next_state=result)
+    
